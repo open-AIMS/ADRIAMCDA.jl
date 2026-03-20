b9a233eebae352e1b9640e0e6f740b6edef2d2a8
Takuya Iwanaga <takuyai@gmail.com>
2026-03-20 23:02:18 +1100

    Robustly handle some methods not providing rankings field

--- src/ADRIAMCDA.jl
+++ src/ADRIAMCDA.jl
@@ -16,7 +16,21 @@ Rankings are returned in "competition" order: Rank 1 indicates "first place".
 """
 function rank_locations(current_conditions::Matrix, prefs::Dict; method=cocoso)
     res = method(current_conditions, prefs[:weights], prefs[:directions])
-    return size(current_conditions, 1) .- res.ranking .+ 1
+
+    # Some methods do not provide a `rankings` field, so have to derive it from the scores.
+    _rankings = try
+        res.ranking
+    catch err
+        if !(err isa FieldError)
+            rethrow(err)
+        end
+
+        # Best score should be rank N, which gets flipped to rank 1 in the return statement
+        _r = sortperm(res.scores; rev=true)
+        size(current_conditions, 1) .- _r .+ 1
+    end
+
+    return size(current_conditions, 1) .- _rankings .+ 1
 end
 function rank_locations(current_conditions::DataFrame, prefs::Dict; method=cocoso)
     return rank_locations(Matrix(current_conditions), prefs; method=method)
